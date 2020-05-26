82f2f50bd824a7b7d20258f245161f018c7f3003
naimtahouri <naimtahouri@gmail.com>
2020-05-26 17:02:50 +0200

    comments

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+// Importation module npm
 const express = require('express')
-var exphbs  = require('express-handlebars');
+const exphbs  = require('express-handlebars');
 
+// Instanciation d'Express 
 const app = express()
 
+// Configuration moteur de template Handlebars et express.static()
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
+// Route GET
 app.get('/', (req,res) => {
     res.render('home', {
         name: "Sylvie"
     })
 })
 
+
+// Application Listen (http://localhost:3000/)
 app.listen(3000, () => {
     console.log('Serveur lancé sur le port 3000')
 })
