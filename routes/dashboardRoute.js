011fc0a19166c88b2421bcc5c2a83380255a5d4c
naimtahouri <naimtahouri@gmail.com>
2020-06-03 23:37:43 +0200

    dashboard update values variable users on vues

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -4,15 +4,21 @@ const connection = require('../config/connectionDb.js')
 
 // Page Dashboard
 dashboardRouter.get('/dashboard/:username', (req,res) => {
+    let usersInfos = []
     let usernameParams = req.params.username
     var sql = `SELECT * FROM users WHERE username = '${usernameParams}'`
     connection.query(sql, function (err, result) {
       if (err) throw err;
-    });
-    res.render('dashboard', {
+      usersInfos.push(...result)
+      console.log('Utilisateur infos envoyé sur le dashboard : ' , usersInfos[0].id)
+      res.render('dashboard', {
         style: '/css/layouts/dashboard.css',
         title: 'Accueil / Twitter',
+        username: usersInfos[0].username,
+        firstname: usersInfos[0].firstname,
+        lastname: usersInfos[0].lastname,
     })
+    });
 })
 
 module.exports = dashboardRouter
