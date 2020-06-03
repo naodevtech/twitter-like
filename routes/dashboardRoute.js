d73bc19fe8f2dcce86ad0018ea54dec0fe443769
naimtahouri <naimtahouri@gmail.com>
2020-06-03 23:12:53 +0200

    front dashboard vue update

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -4,17 +4,14 @@ const connection = require('../config/connectionDb.js')
 
 // Page Dashboard
 dashboardRouter.get('/dashboard/:username', (req,res) => {
-    let array = []
     let usernameParams = req.params.username
     var sql = `SELECT * FROM users WHERE username = '${usernameParams}'`
     connection.query(sql, function (err, result) {
       if (err) throw err;
-      array.push(result[0].username)
     });
     res.render('dashboard', {
         style: '/css/layouts/dashboard.css',
         title: 'Accueil / Twitter',
-        array: array
     })
 })
 
