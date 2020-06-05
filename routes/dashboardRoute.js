4e86e0014ff290e4715aa1ff0d564ceed34f9e1d
My VO <kathy.nguyen198@gmail.com>
2020-06-05 12:17:00 +0200

    enleve toString line 9,10 dashboardRoutes

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -6,8 +6,8 @@ dashboardRouter.get('/dashboard/:username', isAuth, (req,res) => {
  
   user = req.user
   userNameParam = req.params.username;
-  console.log('userNameParam : ' + userNameParam.toString());
-  console.log('userName en session : ' + user.username.toString());
+  console.log('userNameParam : ' + userNameParam);
+  console.log('userName en session : ' + user.username);
 
   if (user.username != userNameParam) {
     console.log('Pas le même utilisateur que la session !!!' );
