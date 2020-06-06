0ef994aa6f1874336754052d6ccaef625b3086e2
naimtahouri <naimtahouri@gmail.com>
2020-06-06 23:13:38 +0200

    home post route passport add

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const homeRouter = express.Router()
+const User = require('../models/User.js')
+const flash = require('connect-flash');
+const passport = require("passport");
+const isAuth = require("../middleware/isAuth");
 
 homeRouter.get('/home', (req,res) => {
     res.render('home', {
@@ -7,5 +11,12 @@ homeRouter.get('/home', (req,res) => {
         title: "Twitter. C'est ce qui se passe dans le monde"
     })
 })
+
+homeRouter.post('/home', passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+)
 
 module.exports = homeRouter
